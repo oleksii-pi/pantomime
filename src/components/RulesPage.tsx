35056f03f31f83d2c4ffc5e0b5bc4fd5ad25a5b9
Aleksey Pi <aleksey.pilyugin@gmail.com>
2024-09-28 14:33:35 +0200

    Add title to Rules page

--- src/components/RulesPage.tsx
+++ src/components/RulesPage.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import TranslationContext from '../TranslationContext';
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import { GrLinkNext } from 'react-icons/gr';
 
 interface RulesPageProps {
@@ -11,26 +11,31 @@ const RulesPage: React.FC<RulesPageProps> = ({ onNext }) => {
   const { t } = useContext(TranslationContext);
 
   return (
-    <div style={{ padding: '20px', textAlign: 'center' }}>
-      <p style={{ fontFamily: 'Roboto, Arial, sans-serif', fontSize: '20px', fontWeight: '300', margin: '0 0 16px 0' }}>
-        {t('game.rules').split('\n').map((line, index) => (
-          <span key={index}>
-            {line}
-            <br />
-          </span>
-        ))}
-      </p>
-      
-      <Button
-        onClick={onNext}
-        variant="contained"
-        color="success"
-        sx={{ fontSize: '24px', padding: '20px 40px' }}
-      >
-        {t('button.play')}
-        <GrLinkNext />
-      </Button>
-    </div>
+      <Box sx={{ 
+          mb: 2, 
+          textAlign: 'center', 
+          mx: { xs: 2, md: 40 } 
+        }}>
+          <h1>{t('title.rules')}</h1>
+        <p style={{ fontFamily: 'Roboto, Arial, sans-serif', fontSize: '20px', fontWeight: '300', margin: '0 0 16px 0' }}>
+          {t('game.rules').split('\n').map((line, index) => (
+            <span key={index}>
+              {line}
+              <br />
+            </span>
+          ))}
+        </p>
+        
+        <Button
+          onClick={onNext}
+          variant="contained"
+          color="success"
+          sx={{ fontSize: '24px', padding: '20px 40px' }}
+        >
+          {t('button.play')}
+          <GrLinkNext />
+        </Button>
+      </Box>
   );
 };
 
